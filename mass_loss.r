22d6d774576942fb7f39c8166fa9ec0e2568034d
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2023-05-17 17:13:53 +0200

    Update mass_loss.r

--- mass_loss.r
+++ mass_loss.r
@@ -12,34 +12,32 @@ setwd("C:/internship/")     # Same version is also on RStudio
 
 ########
 
-# This analysis take into account the categories "initial weight" and "final weight", so it's just descriptive
-raw_data <- read.csv("1_mass_loss/mass_loss_sites_no_bag.csv")     # This is a data frame
-raw_data
+# This data frame contains raw data from the field and the calculation of percentage mass loss
+# Mass loss is calculated as (initial_weight -  final_weight) / initial_weight
 
-str(raw_data)   
+mass_loss <- read.csv("1_mass_loss/mass_loss_sites_percentage.csv")     # This is a data frame
+mass_loss
+
+str(mass_loss)   
 
 # Should I convert the character values into factors? Is it useful?  # They can be categorical (factors are not just atomic vectors, they are objects)
-unique(raw_data$site)
-raw_data$site <- factor(raw_data$site, levels = c("Ellebaekengen", "Stolpehuse", "Trolle", "Lille_Linde", "Skt_Klemens", "Klem_krat", "Skallebanke", "Brahetrolle", "Lyng", "Lyng_krat", "Ommevej", "Ommekrat"))
-str(raw_data$site)
-levels(raw_data$site)
-unique(raw_data$code)
-raw_data$code <- factor(raw_data$code, levels = c("TRNM", "TRNU", "TRRM", "TRRU", "ODNM", "ODNU", "ODRM", "ODRU", "OMNM", "OMNU", "OMRM", "OMRU"))
-str(raw_data$code)
-levels(raw_data$code)
+unique(mass_loss$site)
+mass_loss$site <- factor(mass_loss$site, levels = c("Ellebaekengen", "Stolpehuse", "Trolle", "Lille_Linde", "Skt_Klemens", "Klem_krat", "Skallebanke", "Brahetrolle", "Lyng", "Lyng_krat", "Ommevej", "Ommekrat"))
+str(mass_loss$site)
+levels(mass_loss$site)
+unique(mass_loss$code)
+mass_loss$code <- factor(mass_loss$code, levels = c("TRNM", "TRNU", "TRRM", "TRRU", "ODNM", "ODNU", "ODRM", "ODRU", "OMNM", "OMNU", "OMRM", "OMRU"))
+str(mass_loss$code)
+levels(mass_loss$code)
 
 
 # Summary statistics
-summary(raw_data)
+summary(mass_loss)
 
 
 # What do I have to do with the NAs? Should I remove the rows that contain them?
-# mass_loss <- na.omit(raw_data)
+# mass_loss <- na.omit(mass_loss)
 
-
-########
-
-# This analysis use a file in which I already subtracted the final weight from the initial weight and did the percentage mass loss
 
 
 
