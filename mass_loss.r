1c44638553bbf3bd8c485802efb6426104c1fe92
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2023-05-29 15:04:50 +0200

    Update mass_loss.r

--- mass_loss.r
+++ mass_loss.r
@@ -1,7 +1,6 @@
 # The first step of my analysis is to calculate the percentage mass loss from the litter in the litterbags and the tea in the teabags (green and rooibos)
 # This way I will be able to compare them
 
-# No common garden, only data from the sites
 # For the teas, initial and final weight without the bag are used
 
 
