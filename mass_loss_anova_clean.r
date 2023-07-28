f9e24457db5b483f4c3b0f0b8096f2552501433f
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2023-07-28 16:04:11 +0200

    Update mass_loss_anova_clean.r

--- mass_loss_anova_clean.r
+++ mass_loss_anova_clean.r
@@ -4,7 +4,7 @@
 # First I'll perform a one way anova using treatment as an independent variable
 # Then, I'll perform a two way anova using management and restoration as independent variables
 # The data are mass loss of litter from the sites, litter from the common garden, green tea and red tea
-# Outliers from litter common garden were removed manually
+# Outliers from litter common garden data and red tea data were removed manually
 
 
 
@@ -60,16 +60,26 @@ qqPlot(aov1_red$residuals, id = FALSE)  # QQ-plot
 shapiro.test(aov1_litter$residuals)  # p-value = 0.8736
 shapiro.test(aov1_cg$residuals)  # p-value = 0.007613 (!)
 shapiro.test(aov1_green$residuals)  # p-value = 0.3145
-shapiro.test(aov1_red$residuals)  # p-value = 3.323e-06 (!)
+shapiro.test(aov1_red$residuals)  # p-value = 0.4663
 
 
 ## Checking equality of variances (homogeneity) ##
 
-# Levene's test
+# The Levene test is less sensitive than the Bartlett test to departures from normality. 
+# If you have strong evidence that your data do in fact come from a normal, or nearly 
+# normal, distribution, then Bartlett's test has better performance. 
+
+# Bartlett test (most useful for litter, green tea and red tea)
+bartlett.test(litter_massloss ~ treatment, data = data)  # p-value = 0.1722 (variances are equal)
+bartlett.test(litter_cg_massloss ~ treatment, data = data)  # p-value = 0.004339 (variances are not equal)
+bartlett.test(mean_green ~ treatment, data = data)  # p-value = 0.09873 (variances are equal)
+bartlett.test(mean_red ~ treatment, data = data)  # p-value = 0.9708 (variances are equal)
+
+# Levene's test (most useful for litter common garden)
 leveneTest(litter_massloss ~ treatment, data = data)  # p-value = 0.08503 (variances are equal)
 leveneTest(litter_cg_massloss ~ treatment, data = data)  # p-value = 0.06824 (variances are equal)
 leveneTest(mean_green ~ treatment, data = data)  # p-value = 0.04074 (variances are not equal)
-leveneTest(mean_red ~ treatment, data = data)  # p-value = 0.9162 (variances are equal)
+leveneTest(mean_red ~ treatment, data = data)  # p-value = 0.9564 (variances are equal)
 
 
 # Visual method
@@ -119,7 +129,7 @@ aggregate(mean_red ~ treatment,
 summary(aov1_litter)  # p-value = 0.0291
 summary(aov1_cg)  # p-value = 1.63e-06 
 summary(aov1_green)  # p-value = 0.00616
-summary(aov1_red)  # p-value = 0.773
+summary(aov1_red)  # p-value = 0.162
 
 
 # log transformation of litter common garden
@@ -129,16 +139,16 @@ summary(aov1_cglog) # p-value = 5.12e-06
 shapiro.test(aov1_cglog$residuals)  # p-value = 0.4263
 
 # log transformation of red tea
-data <- mutate(data, logmean_red = log10(mean_red))
-aov1_redlog <- aov(logmean_red ~ treatment, data = data)
-summary(aov1_redlog)  # p-value = 0.658
+# data <- mutate(data, logmean_red = log10(mean_red))
+# aov1_redlog <- aov(logmean_red ~ treatment, data = data)
+# summary(aov1_redlog)  # p-value = 0.658
 
 
 # Report results
 report(aov1_litter)  # The effect of treatment is statistically significant and medium
 report(aov1_cglog)  # The effect of treatment is statistically significant and large
 report(aov1_green)  # The effect of treatment is statistically significant and large
-report(aov1_redlog)  # The effect of treatment is statistically not significant and small
+report(aov1_red)  # The effect of treatment is statistically not significant and medium
 
 
 
@@ -147,13 +157,13 @@ report(aov1_redlog)  # The effect of treatment is statistically not significant 
 TukeyHSD(aov1_litter)  # Only RM-NM are significantly different (p adj = 0.0227204)
 TukeyHSD(aov1_cglog)  # RM-NM (p adj = 0.0000506), RM-NU (p adj = 0.0000118) and RU-RM (p adj = 0.0032223) are significantly different 
 TukeyHSD(aov1_green)  # RM-NM (p adj = 0.0068606) and RM-NU (p adj = 0.0281832) are significantly different
-TukeyHSD(aov1_redlog)  # No combination is significant
+TukeyHSD(aov1_red)  # No combination is significant
 
 # Plot
 plot(TukeyHSD(aov1_litter))
 plot(TukeyHSD(aov1_cglog))
 plot(TukeyHSD(aov1_green))
-plot(TukeyHSD(aov1_redlog))
+plot(TukeyHSD(aov1_red))
 
 
 
@@ -198,7 +208,7 @@ qqPlot(aov2_red$residuals, id = FALSE)  # QQ-plot
 shapiro.test(aov2_litter$residuals)  # p-value = 0.8736
 shapiro.test(aov2_cg$residuals)  # p-value = 0.007613 (!)
 shapiro.test(aov2_green$residuals)  # p-value = 0.3145
-shapiro.test(aov2_red$residuals)  # p-value = 3.323e-06 (!)
+shapiro.test(aov2_red$residuals)  # p-value = 0.4663
 
 
 ## Checking equality of variances (homogeneity) ##
@@ -207,7 +217,7 @@ shapiro.test(aov2_red$residuals)  # p-value = 3.323e-06 (!)
 leveneTest(aov2_litter)  # p-value = 0.08503 (variances are equal)
 leveneTest(aov2_cg)  # p-value = 0.06824 (variances are equal)
 leveneTest(aov2_green)  # p-value = 0.04074 (variances are not equal)
-leveneTest(aov2_red)  # p-value = 0.9162 (variances are equal)
+leveneTest(aov2_red)  # p-value = 0.9564 (variances are equal)
 
 
 # Outliers
@@ -248,7 +258,7 @@ table_green <- group_by(data, restoration, management) %>%
   summarise(
     mean = round(mean(mean_green, na.rm = TRUE), 4),
     sd = round(sd(mean_green, na.rm = TRUE), 4))
-# Litter
+# Red tea
 table_red <- group_by(data, restoration, management) %>%
   summarise(
     mean = round(mean(mean_red, na.rm = TRUE), 4),
@@ -319,15 +329,15 @@ data %>%
 summary(aov2_litter)  # p-value restoration:management = 0.013  (!)
 summary(aov2_cg)  # p-value restoration:management = 0.02689  (!) 
 summary(aov2_green)  # p-value restoration:management = 0.15350
-summary(aov2_red)  # p-value restoration:management = 0.747
+summary(aov2_red)  # p-value restoration:management = 0.6698  
 
 # log transformation of litter from common garden
 aov2_cglog <- aov(loglitter_cg_massloss ~ restoration * management, data = data)
 summary(aov2_cglog)  # p-value = 0.02495 
 
 # log transformation of red tea
-aov2_redlog <- aov(logmean_red ~ restoration * management, data = data)
-summary(aov2_redlog)  # p-value = 0.753
+# aov2_redlog <- aov(logmean_red ~ restoration * management, data = data)
+# summary(aov2_redlog)  # p-value = 0.753
 
 
 ## Two-way ANOVA without interaction ##
@@ -340,15 +350,15 @@ aov2_red_noint <- aov(mean_red ~ restoration + management, data = data)
 summary(aov2_litter_noint)  # p-value restoration = 0.119  # p-value management = 0.567
 summary(aov2_cg_noint)  # p-value restoration = 2.28e-05  # p-value management = 0.0032 
 summary(aov2_green_noint)  # p-value restoration = 0.00189  # p-value management = 0.37930
-summary(aov2_red_noint)  # p-value restoration = 0.971  # p-value management = 0.315
+summary(aov2_red_noint)  # p-value restoration = 0.0578  # p-value management = 0.2314
 
 # log transformation of litter from common garden
 aov2_cglog_noint <- aov(loglitter_cg_massloss ~ restoration + management, data = data)
 summary(aov2_cglog_noint)  # p-value restoration = 4.49e-05   # p-value management = 0.00806 
 
 # log transformation of red tea
-aov2_redlog_noint <- aov(logmean_red ~ restoration + management, data = data)
-summary(aov2_redlog_noint)  # p-value restoration = 0.907   # p-value management = 0.222
+# aov2_redlog_noint <- aov(logmean_red ~ restoration + management, data = data)
+# summary(aov2_redlog_noint)  # p-value restoration = 0.907   # p-value management = 0.222
 
 
 
@@ -357,11 +367,11 @@ summary(aov2_redlog_noint)  # p-value restoration = 0.907   # p-value management
 TukeyHSD(aov2_litter)  # Only restored:managed-near_natural:managed are significantly different (p adj = 0.0227204)
 TukeyHSD(aov2_cglog)  # restored:managed-near_natural:managed (p adj = 0.0000506), near_natural:unmanaged-restored:managed (p adj = 0.0000118) and restored:unmanaged-restored:managed (p adj = 0.0032223) are significantly different
 TukeyHSD(aov2_green)  # restored:managed-near_natural:managed (p adj = 0.0068606) and near_natural:unmanaged-restored:managed (p adj = 0.0281832) are significantly different
-TukeyHSD(aov2_redlog)  # No significant differences
+TukeyHSD(aov2_red)  # No significant differences
 
 
 
-### Visualization ###
+### Visualization ### 1
 
 plitter <- ggplot(table_litter, aes(x = factor(restoration), y = mean, fill = management, colour = management)) + 
   geom_bar(stat = "identity", position = "dodge") + theme(axis.title.x = element_blank()) + ggtitle("Litter from the sites")
@@ -382,5 +392,29 @@ plitter
 pcg
 pgreen
 pred
+dev.off()
+
+
+### Visualization ### 2
+          
+plitter2 <- ggplot(table_litter, aes(x = factor(management), y = mean, fill = restoration, colour = restoration)) + 
+  geom_bar(stat = "identity", position = "dodge") + theme(axis.title.x = element_blank()) + ggtitle("Litter from the sites")
+
+pcg2 <- ggplot(table_cg, aes(x = factor(management), y = mean, fill = restoration, colour = restoration)) + 
+  geom_bar(stat = "identity", position = "dodge") + theme(axis.title.x = element_blank()) + ggtitle("Litter from common garden")
+
+pgreen2 <- ggplot(table_green, aes(x = factor(management), y = mean, fill = restoration, colour = restoration)) + 
+  geom_bar(stat = "identity", position = "dodge") + theme(axis.title.x = element_blank()) + ggtitle("Green tea")
+
+pred2 <- ggplot(table_red, aes(x = factor(management), y = mean, fill = restoration, colour = restoration)) + 
+  geom_bar(stat = "identity", position = "dodge") + theme(axis.title.x = element_blank()) + ggtitle("Red tea")
+
+
+# Plotting all the barplots together
+pdf("1_mass_loss/graphs/mean_differences_3.pdf", width = 12, height = 8)
+plitter2
+pcg2
+pgreen2
+pred2
 dev.off()
 
