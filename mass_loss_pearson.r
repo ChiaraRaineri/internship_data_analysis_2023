51da5c0b8b4a5f6a9a3765a3f37390c38301fffc
ChiaraRaineri <92573129+ChiaraRaineri@users.noreply.github.com>
2023-12-28 11:08:11 +0100

    Update mass_loss_pearson.r

--- mass_loss_pearson.r
+++ mass_loss_pearson.r
@@ -7,6 +7,7 @@ library(car)
 library(report)
 library(viridis)
 library(ggpmisc)
+library(RColorBrewer)
 
 setwd("C:/internship/")
 
@@ -16,6 +17,7 @@ cor.test(data$litter_massloss, data$litter_cg_massloss, method = "pearson")
 # p-value = 8.46e-06 (they are correlated)
 # correlation coefficient = 0.5286027 (positive correlation)
 
+# provisional scatter plot
 ggscatter(data, x = "litter_massloss", y = "litter_cg_massloss", 
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
@@ -26,24 +28,30 @@ cor.test(data$litter_massloss, data$litter_cg_massloss, method = "kendall")
 cor.test(data$litter_massloss, data$litter_cg_massloss, method = "spearman")
 
 
+# shapes and colors (no)
 # https://www.datanovia.com/en/blog/pch-in-r-best-tips/
 shapes = c(15, 16, 17, 18) 
 shapes <- shapes[as.numeric(data$Treatment)]
 plot(data$litter_cg_massloss ~ data$litter_massloss, pch= shapes, col=data$Treatment)
 
-
-ggplot(data, aes(x = litter_massloss, y = litter_cg_massloss, shape = Treatment, colour=Treatment,group = treatment)) +
+ggplot(data, aes(x = litter_massloss, y = litter_cg_massloss, shape = Treatment, colour=Treatment,group = Treatment)) +
   geom_point(size = 3, aes(colour = factor(Treatment))) +
   scale_shape_manual(values = c(15, 16, 17, 18)) + 
   scale_color_viridis(discrete=TRUE) +
   theme_classic()
 
 
-# DEFINITIVO
-ggplot(data, aes(x = litter_massloss, y = litter_cg_massloss, colour=Treatment, group = 1)) +
+# FINAL
+scatter <- ggplot(data, aes(x = litter_massloss, y = litter_cg_massloss, colour=Treatment, group = 1)) +
   geom_point(size = 2, aes(colour = Treatment)) +
-  scale_color_viridis(discrete=TRUE) +
   geom_smooth(method = "lm", se = FALSE, color = "black", fullrange=TRUE, linewidth = 0.5) +
   stat_poly_eq(use_label(c("p", "R2"))) +
   labs(y= "Common garden", x = "Sites") +
   theme_classic()
+
+reg_litter <- scatter + scale_colour_brewer(palette = "Paired")
+
+
+png("1_mass_loss/graphs/scatter_litters.png", res = 150, width = 1500, height = 880)
+reg_litter
+dev.off()
